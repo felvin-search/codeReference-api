828d104fef6998c3fe035b9372efa7ab38ab7214
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-10 18:51:08 +0530

    Added endpoint to Query

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const code = require("./code.json");
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,12 @@ app.use(cors());
 app.options("*", cors());
 app.use(morgan("tiny"));
 
+app.get("/api/query", (req, res) => {
+  const [algorithm, language] = req.body.query;
+  res.send(code[algorithm][language]);//
+});
+
+PORT = process.env.port || process.env.PORT || 5000;
 app.listen(PORT, (err, res) => {
   if (err) {
     console.log(err);
