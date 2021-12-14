c7382c4fd011a3d76bddc6627cd408534de12500
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-14 12:43:33 +0530

    Add use of the The algorithm snippets and rosetta coed as an alternative

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const code = require("./rosetta-code-snippets.json");
+const rosetta_code = require("./rosetta-code-snippets.json");
+const algorithms_code = require("./the-algorithms-snippets.json");
 
 const app = express();
 app.use(express.json());
@@ -10,10 +11,23 @@ app.options("*", cors());
 app.use(morgan("tiny"));
 
 app.get("/api/code", (req, res) => {
+  console.log(req.query);
   const query = req.query.searchQuery;
   const [algorithm, language] = query.split(" in ");
+  let code_snippet;
   try {
-    const response = { code: code[algorithm][language], language: language };
+    //check first if code snippets for the language and algorithm are available in the TheAlgorithms json file
+    if (
+      algorithms_code[language] === undefined ||
+      algorithms_code[language][algorithm] === undefined
+    ) {
+      code_snippet = rosetta_code[algorithm][language];
+      console.log(code_snippet);
+    } else {
+      //If not , use the rosetta code file
+      code_snippet = algorithms_code[language][algorithm];
+    }
+    const response = { code: code_snippet, language: language };
     res.status(200).send(response);
   } catch (err) {
     res.status(404).send({ res: "Query Not found" });
