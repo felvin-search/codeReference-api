556bdbfa1872cf83b92fdac0ac3c40b98b8b085d
rajivharlalka <rajivharlalka009@gmail.com>
2021-12-30 01:46:50 +0530

    Added sql query as a option alongside json queries

--- index.js
+++ index.js
@@ -18,23 +18,6 @@ let db = new sqlite3.Database("./snippets.db", err => {
     console.log("Connected to the snippets database.");
 });
 
-// db.run("CREATE TABLE IF NOT EXISTS snippets (algorithm varchar(255),language varchar(255),snippet varchar)");
-
-// function fill_db() {
-//     for (key of Object.keys(algorithms_code)) {
-//         for (key2 of Object.keys(algorithms_code[key])) {
-//             console.log(key, key2);
-//             db.run("INSERT INTO snippets(algorithm,language,snippet) values(?,?,?)", [
-//                 key2,
-//                 key,
-//                 algorithms_code[key][key2],
-//             ]);
-//         }
-//     }
-// }
-
-// fill_db();
-
 app.get("/api/code", (req, res) => {
     const query = req.query.searchQuery;
     let [algorithm, language] = query.split(" in ");
@@ -69,12 +52,101 @@ app.get("/api/code", (req, res) => {
     }
 });
 
-app.get("/filldb", (req, res) => {
+app.get("/createTable", (req, res) => {
     try {
+        db.run(
+            "CREATE TABLE IF NOT EXISTS snippets (algorithm varchar(255),language varchar(255),snippet varchar,source varchar,name varchar)"
+        );
+        res.send({status: "Table Created"}).status(200);
+    } catch (err) {
+        res.status(400).send({error: err.message});
+    }
+});
+
+app.get("/algorithmsCode", (req, res) => {
+    try {
+        function fill_db() {
+            for (lang of Object.keys(algorithms_code)) {
+                for (algo of Object.keys(algorithms_code[lang])) {
+                    db.run("INSERT INTO snippets(algorithm,language,snippet,source,name) values(?,?,?,?,?)", [
+                        algo,
+                        lang,
+                        algorithms_code[lang][algo],
+                        "https://the-algorithms.com",
+                        "The Algorithm",
+                    ]);
+                }
+            }
+        }
+
+        fill_db();
+        res.send({status: "algorithms code data filled"}).status(200);
+    } catch (err) {
+        res.status(400).send({error: err.message});
+    }
+});
+
+app.get("/rosettaCode", (req, res) => {
+    try {
+        function fill_db() {
+            for (algo of Object.keys(rosetta_code)) {
+                for (lang of Object.keys(rosetta_code[algo])) {
+                    db.run("INSERT INTO snippets(algorithm,language,snippet,source,name) values(?,?,?,?,?)", [
+                        algo,
+                        lang,
+                        rosetta_code[algo][lang],
+                        "http://www.rosettacode.org/wiki/Rosetta_Code",
+                        "Rosetta code",
+                    ]);
+                }
+            }
+        }
+
+        fill_db();
+        res.send({status: "Rosetta code data filled"}).status(200);
+    } catch (err) {
+        res.status(400).send({error: err.message});
+    }
+});
+
+app.get("/api/sql", (req, res) => {
+    try {
+        const query = req.query.searchQuery;
+        let [algorithm, language] = query.split(" in ");
+        if (!language) {
+            language = "javascript";
+        }
+        console.log(algorithm, language);
+        db.all("SELECT * FROM snippets WHERE algorithm=? and language=?", [algorithm, language], (err, rows) => {
+            db.all("SELECT language FROM snippets WHERE algorithm=?", [algorithm], (err, lang_row) => {
+                let languages = [];
+                lang_row.forEach(e => {
+                    languages.push(e.language);
+                });
+                res.send({
+                    snippet: rows[0].snippet,
+                    source: rows[0].source,
+                    name: rows[0].name,
+                    algorithm,
+                    language,
+                    languages,
+                });
+            });
+        });
     } catch (err) {
         res.send({Error: err.message}).status(401);
     }
 });
+
+app.get("/createIndex", (req, res) => {
+    try {
+        db.run("CREATE INDEX idx_algorithm ON snippets (algorithm)");
+        res.status(200).send({status: "Index created"});
+    } catch (err) {
+        res.status(400).send({error: err.message});
+    }
+});
+
 
 PORT = process.env.port || process.env.PORT || 5000;
 app.listen(PORT, (err, res) => {
